603d3be9053bb6b9300a6c6bc717c911713ae1c7
salauddin-sifat-qp <salauddin.sifat@questionpro.com>
2025-05-19 18:11:17 +0600

    add wick ui and msw

---
+++ main-app/eslint.config.js
@@ -0,0 +1,4 @@
+import wickUiEslintConfig from "@npm-questionpro/wick-ui-eslint-config-frontend";
+import tsEslint from "typescript-eslint";
+
+export default tsEslint.config(...wickUiEslintConfig);
---
+++ main-app/src/screens/TodoListScreen.tsx
@@ -0,0 +1,5 @@
+import type React from 'react'
+
+export const TodoListScreen = (): React.JSX.Element => {
+  return <>Todo List</>
+}
